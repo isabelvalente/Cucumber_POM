369101ee1c8b8e5abeb1804ff7996fcdca5280d1
isabellvalente <ivalente@spartaglobal.com>
2018-07-18 17:08:16 +0100

    Homework complete

--- bbc_registration.feature
+++ bbc_registration.feature
@@ -1,1 +1,35 @@
 Feature: BBC Registration
+
+  Scenario: Registering as someone under thirteen years old
+  Given I access the BBC registration page
+  And I click the under thirteen button
+  And I am redirected to the under thirteen registration page
+  When I input parent email
+  Then I should be redirected to a page that says email sent
+
+  Scenario: Registering as someone over thirteen years old with a date that is less than thirteen years ago
+  Given I access the BBC registration page
+  And I click the over thirteen button
+  And I input a date that is less than thirteen years ago
+  When I click continue
+  Then I should see an error message that says I should be thirteen or over
+
+  Scenario: Registering with an invalid email
+  Given I access the BBC registration page as someone over thirteen years old
+  And I enter an email address that does not contain an @ symbol
+  When I click the password field
+  Then I should see an error message that says the email does not look right
+
+  Scenario: Registering with a password that is less thant eight characters long
+  Given I access the BBC registration page as someone over thirteen years old
+  And I input a valid email address
+  And I input a password that is less than eight characters long
+  When I click the postcode field
+  Then I should see an error message that says the password is too short
+
+  Scenario: Registering with a password that has no letters
+  Given I access the BBC registration page as someone over thirteen years old
+  And I input a valid email address
+  And I input a password that contains no letters
+  When I click the postcode field
+  Then I should see an error message that says the password is not valid
--- features/lib/pages/bbc_registration.rb
+++ features/lib/pages/bbc_registration.rb
@@ -10,12 +10,33 @@ class BBC_Registration
   CONFIRM_BDAY_LINK = '#submit-button'
   REGISTRATION_SUBMIT = '#submit-button'
   CONFIRMED = 'Thanks. You\'re now signed in.'
+  PARENT_EMAIL = 'breland.tanveer@lm0k.com'
+  INVALID_EMAIL = 'Ivalenre.com'
 
+  # UNDER 13 REGISTRATION
+  def click_under_13
+    click_link('Under 13')
+  end
+
+  def under_13_register_page
+    visit('https://account.bbc.com/register/details/guardian?context=news&nonce=1U4ipp4q-NsfAFHdtumMMhQ1Y885isiUUqpw&ptrt=https%3A%2F%2Fwww.bbc.co.uk%2Fnews%2Fuk-44871799&userOrigin=news')
+  end
+
+  def fill_parent_email
+    fill_in('Parent or guardian email', with: PARENT_EMAIL)
+  end
+
+  def email_sent_page
+    page.has_content?('Email sent. Now check your inbox and follow the instructions')
+  end
+
+
+  # OVER 13 REGISTRATION
   def click_13_or_over
     click_link('13 or over')
     sleep 2
   end
-
+    # DOB ERRORS
   def fill_in_dob
     fill_in('Day', with: '19')
     sleep 1
@@ -23,7 +44,19 @@ class BBC_Registration
     sleep 1
     fill_in('Year', with: '1994')
     sleep 2
+  end
 
+  def fill_in_wrong_dob
+    fill_in('Day', with: '26')
+    sleep 1
+    fill_in('Month', with: '1')
+    sleep 1
+    fill_in('Year', with: '2008')
+    sleep 1
+  end
+
+  def too_young
+    page.has_content?('Sorry, you need to be 13 or over.')
   end
 
   def click_dob_button
@@ -31,23 +64,60 @@ class BBC_Registration
     sleep 1
   end
 
+    # EMAIL ERRORS
+  def fill_in_invalid_email
+    fill_in('Email', with: INVALID_EMAIL)
+  end
+
   def fill_in_email
     fill_in('Email', with: 'rayan.zayed@lm0k.com')
     sleep 2
   end
 
+  def email_incorrect
+    page.has_content?('Sorry, that email doesn’t look right.')
+  end
+
+
+    # PASSWORD ERRORS
   def fill_in_password
     fill_in('Password', with: 'Acad3my1')
     sleep 2
   end
 
+  def short_password
+    fill_in('Password', with: 'test')
+  end
+
+  def short_password_error
+    page.has_content?('password is too short')
+  end
+
+  def no_letter_password
+    fill_in('Password', with: '12344567')
+  end
+
+  def no_letter_password_error
+    page.has_content?('Sorry, that password isn\'t valid')
+  end
+
+    # POSTCODE ERRORS
   def fill_in_postcode
     fill_in('Postcode', with: 'SE85EQ')
     sleep 1
   end
 
+  def click_postcode
+    find('#postcode-input').click
+  end
+    # GENDER ERRORS
   def select_gender
     select 'Other', from: 'Gender'
+    sleep 1
+  end
+
+  def click_password
+    find('#password-input').click
     sleep 1
   end
 
